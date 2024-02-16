dccc171a82e4ff9d2dd29d0bc3e32fa76b01a8bd
marco-tav <marco.tavella@outlook.com>
2024-02-16 09:49:51 -0300

    Move gameBoard function to a separate module

---
+++ scripts/gameBoard.js
@@ -0,0 +1,65 @@
+function makeGameBoard() {
+  const board = [];
+
+  for(let i=0; i < 3; i++) {
+    board[i] = [];
+    for(let j=0; j < 3; j++) {
+      board[i].push(makeCell());
+    }
+  }
+
+  function placeMarker(row, col, playerMarker) {
+    board[row][col].changeValue(playerMarker);
+  }
+
+  function getRowString(row) {
+    let rowString = "";
+    for(let i = 0; i < 3; i++) {
+      rowString += board[row][i].getValue();
+    }
+
+    return rowString;
+  }
+
+  function getColString(col) {
+    let colString = "";
+    for(let i=0; i < 3; i++) {
+      colString += board[i][col].getValue();
+    }
+
+    return colString;
+  }
+
+  function getDiagonalsArr() {
+    let mainDiagonal = "";
+    let secondaryDiagonal = "";
+
+    for (let i = 0; i < 3; i++) {
+      mainDiagonal += board[i][i].getValue();
+    }
+
+    for (let j = 0; j < 3; j++) {
+      secondaryDiagonal += board[2-j][j].getValue();
+    }
+
+    return [mainDiagonal, secondaryDiagonal];
+  }
+
+  return {placeMarker, getRowString, getColString, getDiagonalsArr}
+}
+
+function makeCell() {
+  let value = "";
+
+  function changeValue(playerMarker) {
+    value = playerMarker;
+  }
+
+  function getValue() {
+    return value;
+  }
+
+  return {changeValue, getValue};
+}
+
+export default makeGameBoard;
